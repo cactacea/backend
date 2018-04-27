18971c6847e3748f5ea190e671c7d82639a30d4b
TAKESHI SHIMADA <>
2018-04-28 00:45:25 +0900

    cactacea-core : Fix session timeout

--- server/src/main/scala/io/github/cactacea/backend/util/filters/AuthFilter.scala
+++ server/src/main/scala/io/github/cactacea/backend/util/filters/AuthFilter.scala
@@ -4,12 +4,13 @@ import com.google.inject.{Inject, Singleton}
 import com.twitter.finagle.http.{Request, Response}
 import com.twitter.finagle.{Service, SimpleFilter}
 import com.twitter.util.Future
+import io.github.cactacea.backend.core.application.components.interfaces.ConfigService
 import io.github.cactacea.backend.util.auth.{AuthTokenGenerator, SessionContext}
 import io.github.cactacea.backend.util.oauth.Permissions
 import io.github.cactacea.backend.core.domain.repositories.SessionsRepository
 
 @Singleton
-class AuthFilter @Inject()(sessionsRepository: SessionsRepository, authTokenGenerator: AuthTokenGenerator) extends SimpleFilter[Request, Response] {
+class AuthFilter @Inject()(sessionsRepository: SessionsRepository, authTokenGenerator: AuthTokenGenerator, configService: ConfigService) extends SimpleFilter[Request, Response] {
 
   override def apply(request: Request, service: Service[Request, Response]): Future[Response] = {
     SessionContext.authenticated match {
@@ -17,7 +18,8 @@ class AuthFilter @Inject()(sessionsRepository: SessionsRepository, authTokenGene
         service(request)
       case false =>
         authTokenGenerator.parse(request.headerMap.get("X-AUTHORIZATION")).flatMap({ auth =>
-          sessionsRepository.checkAccountStatus(auth.sessionId, auth.expiresIn).flatMap({ _ =>
+          val expiresIn = auth.expiresIn - configService.basePointInTime
+          sessionsRepository.checkAccountStatus(auth.sessionId, expiresIn).flatMap({ _ =>
             SessionContext.setAuthenticated(true)
             SessionContext.setId(auth.sessionId)
             SessionContext.setUdid(auth.sessionUdid)
