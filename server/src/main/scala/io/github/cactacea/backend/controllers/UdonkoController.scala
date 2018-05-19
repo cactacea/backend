357eeb5f17ff045a28ba756dcc6513cd94ee9978
TAKESHI SHIMADA <>
2018-05-20 05:12:23 +0900

    cactacea-core : emotion

--- server/src/main/scala/io/github/cactacea/backend/controllers/UdonkoController.scala
+++ server/src/main/scala/io/github/cactacea/backend/controllers/UdonkoController.scala
@@ -55,14 +55,14 @@ class UdonkoController @Inject()(db: DatabaseService) extends Controller {
     val a2 = e.faceRectangle.left.getOrElse(0L)
     val a3 = e.faceRectangle.width.getOrElse(0L)
     val a4 = e.faceRectangle.height.getOrElse(0L)
-    val a5 = e.scores.anger.getOrElse(0.0f)
-    val a6 = e.scores.contempt.getOrElse(0.0f)
-    val a7 = e.scores.disgust.getOrElse(0.0f)
-    val a8 = e.scores.fear.getOrElse(0.0f)
-    val a9 = e.scores.happiness.getOrElse(0.0f)
-    val a10 = e.scores.neutral.getOrElse(0.0f)
-    val a11 = e.scores.sadness.getOrElse(0.0f)
-    val a12 = e.scores.surprise.getOrElse(0.0f)
+    val a5 = e.faceAttributes.emotion.anger.getOrElse(0.0f)
+    val a6 = e.faceAttributes.emotion.contempt.getOrElse(0.0f)
+    val a7 = e.faceAttributes.emotion.disgust.getOrElse(0.0f)
+    val a8 = e.faceAttributes.emotion.fear.getOrElse(0.0f)
+    val a9 = e.faceAttributes.emotion.happiness.getOrElse(0.0f)
+    val a10 = e.faceAttributes.emotion.neutral.getOrElse(0.0f)
+    val a11 = e.faceAttributes.emotion.sadness.getOrElse(0.0f)
+    val a12 = e.faceAttributes.emotion.surprise.getOrElse(0.0f)
 
     val q = quote {
       query[expressions].insert(
@@ -107,13 +107,17 @@ case class GetFriend(value: Float)
 case class PostHeart(beat: Float)
 case class PostExpression(
                            faceRectangle: PostExpressionFaceRectangle,
-                           scores: PostExpressionScores
+                           faceAttributes: PostExpressionEmotion
                          )
+
 case class PostExpressionFaceRectangle(
                                         top: Option[Long],
                                         left: Option[Long],
                                         width: Option[Long],
                                         height: Option[Long])
+
+case class PostExpressionEmotion(emotion: PostExpressionScores)
+
 case class PostExpressionScores(
                                  anger: Option[Float],
                                  contempt: Option[Float],
