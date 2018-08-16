0d6f123e73ed55a69d2f95c8723fd8edcc1fe7fc
TAKESHI SHIMADA <>
2018-08-17 00:41:28 +0900

    cactacea-backend : Fix CorsFilter

--- server/src/main/scala/io/github/cactacea/backend/utils/filters/CorsFilter.scala
+++ server/src/main/scala/io/github/cactacea/backend/utils/filters/CorsFilter.scala
@@ -2,7 +2,7 @@ package io.github.cactacea.backend.utils.filters
 
 import com.google.inject.Singleton
 import com.twitter.finagle.http.{Request, Response}
-import com.twitter.finagle.{Service, SimpleFilter}
+import com.twitter.finagle.{Service, SimpleFilter, http}
 import com.twitter.util.Future
 
 @Singleton
@@ -10,12 +10,16 @@ class CorsFilter extends SimpleFilter[Request, Response] {
   override def apply(request: Request, service: Service[Request, Response]): Future[Response] = {
    service(request).map {
       response =>
-        response.headerMap
-          .add("access-control-allow-origin", "*")
-          .add("access-control-allow-headers", "accept, content-type")
-          .add("access-control-allow-methods", "GET,HEAD,POST,DELETE,OPTIONS,PUT,PATCH")
-
-        response
+        if (request.method.name == "OPTIONS") {
+          response.statusCode(http.Status.Ok.code)
+        } else {
+          response.headerMap
+            .set("Access-Control-Allow-Credentials", "true")
+            .set("Access-Control-Allow-Headers", "Origin, Authorization, Accept")
+            .set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE, PUT")
+            .set("Access-Control-Allow-Origin", "*")
+          response
+        }
     }
   }
 }
