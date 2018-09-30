fc7019639f22dbdb463ae84a34c2367e3bad3cae
TAKESHI SHIMADA <>
2018-09-30 18:34:27 +0900

    Fix generate_sdk.sh

--- generate_sdks.sh
+++ generate_sdks.sh
@@ -1,2 +1,15 @@
 #!/usr/bin/env bash
-swagger-codegen generate -i docs/swagger.json -l swift4 --language-specific-primitives Int --type-mappings integer=Int -o ../ios --additional-properties projectName=Cactacea,podAuthors=takeshishimada,responseAs=RxSwift
+swagger-codegen generate \
+    -i docs/swagger.json \
+    -l swift4 \
+    --type-mappings number=Int64 \
+    --language-specific-primitives Int16,Int32,Int64,NSDate \
+    -o ../ios --additional-properties projectName=Cactacea,podAuthors=takeshishimada,responseAs=RxSwift
+
+
+swagger-codegen generate \
+    -i docs/swagger.json  \
+    -l kotlin
+    --model-package io.github.cactacea.backend \
+    --model-name-suffix Json
+    -o ../android
