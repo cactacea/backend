5d51b661a03e6e786bde793b0c91c0a1686b06db
TAKESHI SHIMADA <>
2018-09-30 22:19:01 +0900

    Fix tests

--- core/src/test/scala/io/github/cactacea/core/infrastructure/dao/AccountFeedsDAOSpec.scala
+++ core/src/test/scala/io/github/cactacea/core/infrastructure/dao/AccountFeedsDAOSpec.scala
@@ -17,9 +17,9 @@ class AccountFeedsDAOSpec extends DAOSpec {
     val sessionAccount4 = createAccount("AccountFeedsDAOSpec3")
     val sessionAccount5 = createAccount("AccountFeedsDAOSpec4")
     val sessionAccount6 = createAccount("AccountFeedsDAOSpec5")
-    val medium1 = this.createMedium(sessionAccount1.id)
-    val medium2 = this.createMedium(sessionAccount1.id)
-    val medium3 = this.createMedium(sessionAccount1.id)
+    val medium1 = createMedium(sessionAccount1.id)
+    val medium2 = createMedium(sessionAccount1.id)
+    val medium3 = createMedium(sessionAccount1.id)
     val message = "message"
     val mediums = List(medium1.id, medium2.id, medium3.id)
     val tags = List("tag1", "tag2", "tag3")
@@ -27,17 +27,21 @@ class AccountFeedsDAOSpec extends DAOSpec {
     val contentWarning = true
 
     // create feed
-    val feedId = Await.result(feedsDAO.create(message, Some(mediums), Some(tags), privacyType, contentWarning, None, sessionAccount2.id.toSessionId))
+    val feedId = Await.result(db.transaction(feedsDAO.create(message, Some(mediums), Some(tags), privacyType, contentWarning, None, sessionAccount2.id.toSessionId)))
 
     // create follows
-    Await.result(followersDAO.create(sessionAccount2.id, sessionAccount1.id.toSessionId))
-    Await.result(followersDAO.create(sessionAccount2.id, sessionAccount3.id.toSessionId))
-    Await.result(followersDAO.create(sessionAccount2.id, sessionAccount4.id.toSessionId))
-    Await.result(followersDAO.create(sessionAccount2.id, sessionAccount5.id.toSessionId))
-    Await.result(followersDAO.create(sessionAccount2.id, sessionAccount6.id.toSessionId))
+    Await.result(db.transaction(
+      for {
+        _ <- followersDAO.create(sessionAccount2.id, sessionAccount1.id.toSessionId)
+        _ <- followersDAO.create(sessionAccount2.id, sessionAccount3.id.toSessionId)
+        _ <- followersDAO.create(sessionAccount2.id, sessionAccount4.id.toSessionId)
+        _ <- followersDAO.create(sessionAccount2.id, sessionAccount5.id.toSessionId)
+        _ <- followersDAO.create(sessionAccount2.id, sessionAccount6.id.toSessionId)
+      } yield (Unit)
+    ))
 
     // create account feeds
-    Await.result(accountFeedsDAO.create(feedId, sessionAccount2.id.toSessionId))
+    Await.result(db.transaction(accountFeedsDAO.create(feedId, sessionAccount2.id.toSessionId)))
 
     val q = quote { query[AccountFeeds].filter(_.feedId == lift(feedId)).size}
     val result = Await.result(db.run(q))
@@ -53,9 +57,9 @@ class AccountFeedsDAOSpec extends DAOSpec {
     val sessionAccount4 = createAccount("AccountFeedsDAOSpec9")
     val sessionAccount5 = createAccount("AccountFeedsDAOSpec10")
     val sessionAccount6 = createAccount("AccountFeedsDAOSpec11")
-    val medium1 = this.createMedium(sessionAccount1.id)
-    val medium2 = this.createMedium(sessionAccount1.id)
-    val medium3 = this.createMedium(sessionAccount1.id)
+    val medium1 = createMedium(sessionAccount1.id)
+    val medium2 = createMedium(sessionAccount1.id)
+    val medium3 = createMedium(sessionAccount1.id)
     val message = "message"
     val mediums = List(medium1.id, medium2.id, medium3.id)
     val tags = List("tag1", "tag2", "tag3")
@@ -63,17 +67,23 @@ class AccountFeedsDAOSpec extends DAOSpec {
     val contentWarning = true
 
     // create feed
-    val feedId = Await.result(feedsDAO.create(message, Some(mediums), Some(tags), privacyType, contentWarning, None, sessionAccount2.id.toSessionId))
+    val feedId = Await.result(db.transaction(feedsDAO.create(message, Some(mediums), Some(tags), privacyType, contentWarning, None, sessionAccount2.id.toSessionId)))
 
     // create follows
-    Await.result(followersDAO.create(sessionAccount2.id, sessionAccount1.id.toSessionId))
-    Await.result(followersDAO.create(sessionAccount2.id, sessionAccount3.id.toSessionId))
-    Await.result(followersDAO.create(sessionAccount2.id, sessionAccount4.id.toSessionId))
-    Await.result(followersDAO.create(sessionAccount2.id, sessionAccount5.id.toSessionId))
-    Await.result(followersDAO.create(sessionAccount2.id, sessionAccount6.id.toSessionId))
+    Await.result(
+      db.transaction(
+        for {
+          _ <- followersDAO.create(sessionAccount2.id, sessionAccount1.id.toSessionId)
+          _ <- followersDAO.create(sessionAccount2.id, sessionAccount3.id.toSessionId)
+          _ <- followersDAO.create(sessionAccount2.id, sessionAccount4.id.toSessionId)
+          _ <- followersDAO.create(sessionAccount2.id, sessionAccount5.id.toSessionId)
+          _ <- followersDAO.create(sessionAccount2.id, sessionAccount6.id.toSessionId)
+        } yield (Unit)
+      )
+    )
 
     // create account feeds
-    Await.result(accountFeedsDAO.create(feedId, sessionAccount2.id.toSessionId))
+    Await.result(db.transaction(accountFeedsDAO.create(feedId, sessionAccount2.id.toSessionId)))
 
     val ids =  List(
       sessionAccount1.id,
@@ -84,7 +94,7 @@ class AccountFeedsDAOSpec extends DAOSpec {
     )
 
     // update notified
-    Await.result(accountFeedsDAO.update(feedId, ids, true))
+    Await.result(db.transaction(accountFeedsDAO.update(feedId, ids, true)))
 
     //
     val q = quote { query[AccountFeeds].filter(_.feedId == lift(feedId)).filter(_.notified == true).size}
