8b5a9377984eb723f1208ff518317cee31dad8da
TAKESHI SHIMADA <>
2018-11-17 11:14:09 +0900

    Switch to OpenAPI Generator

--- generate_sdks.sh
+++ generate_sdks.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
 # Fixme
-swagger-codegen generate \
+openapi-generator generate \
     -i docs/swagger.json \
-    -l swift4 \
+    -g swift4 \
     --type-mappings number=Int64 \
     --language-specific-primitives Int16,Int32,Int64,NSDate \
     --additional-properties projectName="Cactacea" \
-    --additional-properties podVersion="18.11.2" \
+    --additional-properties podVersion="18.11.4" \
     --additional-properties podSummary="Cactacea iOS SDK" \
     --additional-properties podDescription="An iOS library to interface with Cactacea backend API" \
     --additional-properties podSource="" \
@@ -15,11 +15,13 @@ swagger-codegen generate \
     --additional-properties podLicense="" \
     --additional-properties podHomepage=https://github.com/cactacea/ios \
     --additional-properties responseAs=RxSwift \
+    --skip-validate-spec \
     -o ../ios
 
-swagger-codegen generate \
+openapi-generator generate \
     -i docs/swagger.json  \
-    -l kotlin \
+    -g kotlin \
     --model-package io.github.cactacea.backend \
     --model-name-suffix Json \
+    --skip-validate-spec \
     -o ../android
