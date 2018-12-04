833a57cd34b43c0d4664f467216428834750e7da
TAKESHI SHIMADA <>
2018-12-05 00:01:32 +0900

    Add demo data

--- demo/src/main/scala/io/github/cactacea/backend/DemoSetup.scala
+++ demo/src/main/scala/io/github/cactacea/backend/DemoSetup.scala
@@ -1,12 +1,14 @@
 package io.github.cactacea.backend
 
+import java.nio.file.{Files, Paths}
+
 import io.github.cactacea.backend.core.util.configs.Config
 
 import scala.collection.JavaConverters._
 
 object DemoSetup {
 
-  def execute(): Unit = {
+  def migrate(): Unit = {
 
     val database = Config.db.master.database
     val user = Config.db.master.user
@@ -25,5 +27,32 @@ object DemoSetup {
 
   }
 
+  def setupData(localPath: String): Unit = {
+    copyFile(localPath, "3d481e5c-a8cf-48b5-9b0b-dd131c55058a")
+    copyFile(localPath, "8ca7db4e-e53e-4e03-b02f-a308159badf1")
+    copyFile(localPath, "8ed1d124-95a1-429c-9cdd-5fdf870fc2a2")
+    copyFile(localPath, "9bd131cd-c576-4c78-ae2e-63e90597a89e")
+    copyFile(localPath, "9db4e781-981c-4d5b-a774-f752a0a54cfd")
+    copyFile(localPath, "17d1c3eb-09d5-4dc3-8412-4bdcdcaadadd")
+    copyFile(localPath, "26d273ea-928f-4dc5-b700-4dace2f29d1c")
+    copyFile(localPath, "39fb44d3-5524-4cee-ba4d-50ca0e2a051a")
+    copyFile(localPath, "65beecd6-d78b-4805-9026-f96cb064b500")
+    copyFile(localPath, "74116607-2a0f-4b3c-b0f4-75dea7331c7d")
+    copyFile(localPath, "a226019d-dc00-4e97-b83f-d460e368113d")
+    copyFile(localPath, "b545bf2d-f11d-4346-9898-33d7aac627ed")
+    copyFile(localPath, "b643b26f-0ba1-46b3-965e-6c22ca471be1")
+    copyFile(localPath, "c87864bb-31e4-40de-b7fd-f2e22801ba4b")
+    copyFile(localPath, "e77ba2bc-6c2a-49ca-807f-6571159bcf78")
+    copyFile(localPath, "e9905655-ed5a-4842-986d-16dcf85b7ba1")
+    copyFile(localPath, "f65d141e-f95a-40cd-b4df-ef6a2035d8ab")
+    copyFile(localPath, "fc20ae30-2b36-42b3-ad88-868ee73cf8ae")
+  }
+
+  private def copyFile(localPath: String, resourceName: String): Unit = {
+    val origin = Paths.get(this.getClass.getClassLoader.getResource(s"demo/images/$resourceName").toURI)
+    val tmp = Paths.get(s"$localPath/$resourceName")
+    Files.deleteIfExists(tmp)
+    Files.copy(origin, tmp)
+  }
 
 }
--- demo/src/main/scala/io/github/cactacea/backend/DemoSetupHandler.scala
+++ demo/src/main/scala/io/github/cactacea/backend/DemoSetupHandler.scala
@@ -1,84 +1,18 @@
 package io.github.cactacea.backend
 
-import java.nio.file.{Files, Paths}
-import java.util.UUID
-
 import com.google.inject.{Inject, Singleton}
-import com.twitter.finagle.http.{FileElement, RequestBuilder}
 import com.twitter.finatra.http.routing.HttpWarmup
-import com.twitter.finatra.httpclient.RequestBuilder._
 import com.twitter.finatra.json.FinatraObjectMapper
+import com.twitter.inject.annotations.Flag
 import com.twitter.inject.utils.Handler
-import com.twitter.io.Buf
-import io.github.cactacea.backend.core.util.configs.Config
-import io.github.cactacea.backend.models.requests.session.PutSessionProfileImage
-import io.github.cactacea.backend.models.requests.sessions.PostSignUp
-import io.github.cactacea.backend.models.responses.{Authentication, MediumCreated}
 
 @Singleton
-class DemoSetupHandler  @Inject()(mapper: FinatraObjectMapper, warmup: HttpWarmup) extends Handler {
+class DemoSetupHandler  @Inject()(@Flag("storage.localPath") localPath: String, mapper: FinatraObjectMapper, warmup: HttpWarmup) extends Handler {
+
 
   override def handle(): Unit = {
-    DemoSetup.execute()
-
-    try {
-      createAccount("ito_hirobumi", "Password_2018")
-      createAccount("kuroda_kiyotaka", "Password_2018")
-      createAccount("sanjo_sanetomi", "Password_2018")
-      createAccount("yamagata_aritomo", "Password_2018")
-      createAccount("matsutaka_masayoshi", "Password_2018")
-      createAccount("okuma_shigenobu", "Password_2018")
-      createAccount("saionji_kinmochi", "Password_2018")
-      createAccount("katsura_taro", "Password_2018")
-      createAccount("terauchi_masatake", "Password_2018")
-      createAccount("hara_takashi", "Password_2018")
-      createAccount("uchida_kosai", "Password_2018")
-      createAccount("takahashi_korekiyo", "Password_2018")
-      createAccount("kato_tomosaburo", "Password_2018")
-      createAccount("yamamoto_gonbei", "Password_2018")
-      createAccount("kiyoura_keigo", "Password_2018")
-      createAccount("kato_takaaki", "Password_2018")
-      createAccount("wakatsuki_reijiro", "Password_2018")
-      createAccount("tanaka_giichi", "Password_2018")
-
-    } catch {
-      case e: Throwable =>
-        println(e)
-    }
-
-  }
-
-  def createAccount(accountName: String, password: String): Unit = {
-    val agent = Some("Mozilla/5.0 (iPhone; CPU iPhone OS 11_4_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/11.0 Mobile/15E148 Safari/604.1")
-    val uuid = UUID.randomUUID().toString
-    val postSignUp = PostSignUp(accountName, password, uuid, agent)
-    val body = mapper.writePrettyString(postSignUp)
-    val request = post("/sessions").body(body).header(Config.auth.headerNames.apiKey, Config.auth.keys.all.head._2)
-
-    warmup.send(request, false, 1, { response =>
-      val auth = mapper.parse[Authentication](response.contentString)
-      val bytes = Files.readAllBytes(Paths.get(this.getClass.getClassLoader.getResource(s"$accountName.jpg").toURI))
-
-      // TODO
-      val request = RequestBuilder().url("http://localhost:9000/mediums")
-        .add(FileElement("file", Buf.ByteArray.Owned(bytes), Some("image/jpeg"), Some("marathon.jpg")))
-        .addHeader(Config.auth.headerNames.apiKey, Config.auth.keys.all.head._2)
-        .addHeader(Config.auth.headerNames.authorizationKey, auth.accessToken)
-        .buildFormPost(multipart = true)
-
-      warmup.send(request, false, 1, { response =>
-        val medium = mapper.parse[MediumCreated](response.contentString)
-        val putSessionProfileImage = PutSessionProfileImage(medium.id)
-        val body = mapper.writePrettyString(putSessionProfileImage)
-        val request = put("/session/profile_image").body(body)
-          .header(Config.auth.headerNames.apiKey, Config.auth.keys.all.head._2)
-          .header(Config.auth.headerNames.authorizationKey, auth.accessToken)
-        warmup.send(request, false, 1, { response =>
-          println(response.contentString)
-        })
-      })
-    })
-
+    DemoSetup.migrate()
+    DemoSetup.setupData(localPath)
   }
 
 
