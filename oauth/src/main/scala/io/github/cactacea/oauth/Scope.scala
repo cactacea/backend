569b892a32e320cde9d5e86f13d10c93e6c5690d
TAKESHI SHIMADA <>
2019-07-25 13:01:17 +0900

    Change scope name

--- oauth/src/main/scala/io/github/cactacea/oauth/Scope.scala
+++ oauth/src/main/scala/io/github/cactacea/oauth/Scope.scala
@@ -10,11 +10,11 @@ object Scope {
   final object comments extends Scope(1 << 3, "comments"                     , "to post and delete comments on a user’s behalf") // scalastyle:ignore
   final object commentLikes extends Scope(1 << 4, "comment_likes"           , "to like and unlike comment on a user’s behalf") // scalastyle:ignore
   final object groups extends Scope(1 << 5, "groups"                        , "to create and delete groups on a user's behalf") // scalastyle:ignore
-  final object invitations extends Scope(1 << 6, "group_invitations"   , "to create, delete, accept and reject invitations on a user's behalf") // scalastyle:ignore
+  final object invitations extends Scope(1 << 6, "invitations"   , "to create, delete, accept and reject invitations on a user's behalf") // scalastyle:ignore
   final object messages extends Scope(1 << 7, "messages"                   , "to post and delete messages on a user's behalf") // scalastyle:ignore
   final object followerList extends Scope(1 << 8, "follower_list"          , "to read the list of followers and followed-by users") // scalastyle:ignore
   final object relationships extends Scope(1 << 9, "relationships"         , "to follows and unfollow accounts on a user’s behalf") // scalastyle:ignore
-  final object friendRequests extends Scope(1 << 10, "friend_requests"     , "to create and delete friend request on a user’s behalf") // scalastyle:ignore
+  final object requests extends Scope(1 << 10, "requests"     , "to create and delete friend request on a user’s behalf") // scalastyle:ignore
   final object media extends Scope(1 << 11, "media"                        , "to create and delete media on a user’s behalf") // scalastyle:ignore
   final object reports extends Scope(1 << 12, "reports"                    , "to report account, feed, group and comment on a user’s behalf") // scalastyle:ignore
 
@@ -29,7 +29,7 @@ object Scope {
     messages,
     followerList,
     relationships,
-    friendRequests,
+    requests,
     media,
     reports
   )
